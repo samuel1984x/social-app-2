a8fa4ed1b3a5f47a988faec7f4d9315169710724
Yoav Slama <yoav.slama@gmail.com>
2025-12-27 21:24:18 +0200

    Add Post mongoose model

---
+++ models/post_model.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema(
+  {
+    message: { type: String, required: true },
+    sender: { type: String, required: true }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Post", postSchema);
